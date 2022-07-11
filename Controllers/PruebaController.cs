b627212f3086dfeeb85ab81952681804089407ab
Alberto Pérez Fructuoso <alberto@gestycloud.com>
2022-07-11 10:34:15 +0200

    first commit

---
+++ Controllers/PruebaController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;  
+  
+namespace SampleMVCApps.Controllers  
+{  
+    public class PruebaController:Controller  
+    {  
+        public IActionResult Index()  
+        {     
+            ViewData["Message"]="Hello, This is my view";  
+            return View();  
+        }  
+  
+        public IActionResult Prueba()  
+        {  
+            ViewData["Message"]="Hello, Welcome to HelloWorld Application";  
+            return View();  
+        }  
+    }  
+}  
